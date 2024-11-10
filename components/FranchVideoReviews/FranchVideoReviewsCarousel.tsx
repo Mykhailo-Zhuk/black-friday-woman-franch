e5bf6cf29aebe0a3422dd6da33d277813edd77e1
Mykhailo-Zhuk <mishazhuk1@gmail.com>
2024-11-10 23:26:31 +0200

    completed mobile version

--- components/FranchVideoReviews/FranchVideoReviewsCarousel.tsx
+++ components/FranchVideoReviews/FranchVideoReviewsCarousel.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef, useState } from "react";
+import React from "react";
 import { EmblaOptionsType } from "embla-carousel";
 import { DotButton, useDotButton } from "../OurFranchise/EmblaCarouselDotButtons";
 import useEmblaCarousel from "embla-carousel-react";
